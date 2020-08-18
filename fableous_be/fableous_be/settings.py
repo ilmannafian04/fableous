c29cff7dedc7449799e221f1a83a8919e97cc933
Ilman Nafian <milmannafian04@gmail.com>
2020-08-18 12:22:59 +0700

    add requirements.txt

--- fableous_be/fableous_be/settings.py
+++ fableous_be/fableous_be/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# noinspection PyPackageRequirements
 from environ import environ
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 env = environ.Env(

d5b7b8a354121ac28d9eedbf5603ff223897065b
Ilman Nafian <milmannafian04@gmail.com>
2020-09-06 16:52:14 +0700

    connect to secure ws

--- fableous_fe/src/containers/CanvasDraw/DrawingSession.jsx
+++ fableous_fe/src/containers/CanvasDraw/DrawingSession.jsx
@@ -23,7 +23,7 @@ const DrawingSession = () => {
     }, []);
     useEffect(() => {
         if (roomCode) {
-            const sock = new WebSocket(`ws://deco3801-todo-team-name.uqcloud.net/ws/drawing/${roomCode}/`);
+            const sock = new WebSocket(`wss://deco3801-todo-team-name.uqcloud.net/ws/drawing/${roomCode}/`);
             sock.onopen = () => {
                 setSocket(sock);
             };

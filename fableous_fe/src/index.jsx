471345922659be21b44d493ed582079b64fbff45
Ilman Nafian <milmannafian04@gmail.com>
2020-09-06 13:55:41 +0700

    use prod url instead of development

--- fableous_fe/src/index.jsx
+++ fableous_fe/src/index.jsx
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom';
 import App from './containers/App/App';
 import './index.css';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://deco3801-todo-team-name.uqcloud.net';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 
 ReactDOM.render(
     <React.StrictMode>

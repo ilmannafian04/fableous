76fac6e31d2d469dc8690b2cb95b073df94a0b75
Muhammad Naufal Irbahhana <irbahananaufal@gmail.com>
2020-10-20 17:56:05 +0700

    finish styling sign in and sign up

--- fableous_fe/src/containers/authentication/SignIn.jsx
+++ fableous_fe/src/containers/authentication/SignIn.jsx
@@ -1,7 +1,70 @@
 import React, { useState } from 'react';
 import Axios from 'axios';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        root: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100vw',
+            height: '100vh',
+            backgroundColor: '#2E3138',
+        },
+        paper: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '70%',
+            height: '70%',
+            backgroundColor: '#F6F1D3',
+            borderRadius: '46px',
+        },
+        paperInside: {
+            width: '97%',
+            height: '97%',
+            backgroundColor: '#7030A2',
+            margin: 'inherit',
+            borderRadius: '46px',
+        },
+        input: {
+            backgroundColor: '#FFF',
+            borderRadius: '5px',
+            marginBottom: '5px',
+        },
+        formWrapper: {
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        inputWrapper: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        content: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            flexDirection: 'column',
+        },
+        title: {
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: '1rem',
+        },
+    })
+);
 
 export const SignIn = () => {
+    const classes = useStyles();
     const [formState, setFormState] = useState({ username: '', password: '' });
     const changeHandler = (event) => {
         const target = event.target;
@@ -27,11 +90,45 @@ export const SignIn = () => {
             });
     };
     return (
-        <form onSubmit={submitHandler}>
-            <input type="text" name="username" onChange={changeHandler} />
-            <input type="password" name="password" onChange={changeHandler} />
-            <button type="submit">Sign In</button>
-        </form>
+        <div className={classes.root}>
+            <div className={classes.paper}>
+                <div className={classes.paperInside}>
+                    <div className={classes.content}>
+                        <div className={classes.title}>
+                            <Typography variant={'h3'} style={{ color: '#F6F1D3', fontWeight: 700 }}>
+                                Fableous
+                            </Typography>
+                            <Typography variant={'h5'} style={{ color: '#F6F1D3', fontWeight: 500 }}>
+                                Sign in
+                            </Typography>
+                        </div>
+                        <div className={classes.inputWrapper}>
+                            <form onSubmit={submitHandler} className={classes.formWrapper}>
+                                <TextField
+                                    className={classes.input}
+                                    variant="filled"
+                                    type="text"
+                                    label="username"
+                                    name="username"
+                                    onChange={changeHandler}
+                                />
+                                <TextField
+                                    className={classes.input}
+                                    variant="filled"
+                                    type="password"
+                                    label="password"
+                                    name="password"
+                                    onChange={changeHandler}
+                                />
+                                <Button type={'submit'} variant="contained" color="primary">
+                                    Sign In
+                                </Button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
